38da5bb083e1a593e17b0dd1d9b699fcde651675
jishnn tp <jishnn.tp@gmail.com>
2021-02-22 19:03:09 +0530

    startup script v2

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install apache2 -y
-sudo apt-get install git -y
-git clone https://github.com/Schrodinger-kat/google-cloud-activity.git
-cd google-cloud-activity/wordpress/
+sudo apt install -y apache2 
+sudo apt-get install -y git 
+git clone https://github.com/Schrodinger-kat/terraform-wordpress_app.git
+cd terraform-wordpress_app
 sudo cp 000-default.conf /etc/apache2/sites-enabled/
 sudo service apache2 restart
 cd /var/www/
 sudo mv html public_html
-sudo apt-get install php7.0 php-pear php7.0-mysql
+sudo apt-get install -y php7.0 php-pear php7.0-mysql
 sudo service apache2 restart
 cd /var/www/public_html
 #for debugging purpose
@@ -19,7 +19,7 @@ sudo mkdir jishnn
 cd jishnn
 sudo wget https://wordpress.org/latest.zip
 sudo apt-get install unzip -y
-unzip latest.zip
+sudo unzip latest.zip
 
 sudo chown -R www-data /var/www/public_html
 sudo chmod -R 755 /var/www/public_html
